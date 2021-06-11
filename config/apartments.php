2c594cd14a7271868e0b1028a08862ed4ff932d0
Matteo <matteomigliori280489@gmail.com>
2021-06-11 20:25:31 +0200

    migration apartment

---
+++ config/apartments.php
@@ -0,0 +1,86 @@
+<?php
+
+ return [
+    [
+        'title'=> 'Mountain Chalet Milly',
+        'rooms_n'=>4,
+        'beds_n'=>2,
+        'bathroom'=>1,
+        'dimension'=>85,
+        'latitude'=>'',
+        'longitude'=>'',
+        'description'=>'Appartamento su due piani in casa bifamiliare 4 posti letto:
+            ampia camera matrimoniale al primo piano ;al piano inferiore cucina superaccessiorata , salotto/soggiorno con divano letto e camino , bagno
+            Garage e giardino attrezzato.
+            In posizione soleggiata (500 m s.l.m.) dista 5 km da Sondrio.
+            Ideale punto d\'appoggio per la pratica di numerosi SPORT come sci /climbing/biking/golf/canoa/tennis trekking ',
+        'visible'=>'',
+        'price_per_night'=>155.00,
+        'rating'=>4.7,
+    ],
+
+    [
+        'title'=> 'La Baita Case Suite',
+        'rooms_n'=>3,
+        'beds_n'=>2,
+        'bathroom'=>1,
+        'dimension'=>60,
+        'latitude'=>'',
+        'longitude'=>'',
+        'description'=>'L\'appartamento LaBaitaCase Suite è situato nel centro storico di Sondrio a due passi da Piazza Garibaldi, sede del Teatro Sociale, dal MVSA (Museo Valtellinese di Storia ed Arte) e delle sedi delle Banche locali e non.
+         Sono nelle immediate vicinanze bar, ristoranti, negozi, farmacie e i principali servizi.',
+        'visible'=>'',
+        'price_per_night'=>119.99,
+        'rating'=>4.4,
+    ],
+    [
+        'title'=> 'Loft Caterina',
+        'rooms_n'=>4,
+        'beds_n'=>5,
+        'bathroom'=>2,
+        'dimension'=>105,
+        'latitude'=>'',
+        'longitude'=>'',
+        'description'=>'Splendido loft, interamente rivestito in legno caldo, accogliente e luminoso con magnifica vista sulle piste dell\'Abetone.
+        Un unico spazio dove è stata ricavata anche una camera matrimoniale separata con bagno en suite. Altro bagno per gli ospiti nel corridoio di ingresso e posti letto in salotto. Cucina piccola ma funzionale.
+        Sicuramente un posto ideale dove passare le vacanze in convivialità con la famiglia o gli amici.',
+        'visible'=>'',
+        'price_per_night'=>180.00,
+        'rating'=>4.6,
+    ],
+    [
+        'title'=> 'La Casa di Alice',
+        'rooms_n'=>5,
+        'beds_n'=>2,
+        'bathroom'=>2,
+        'dimension'=>88,
+        'latitude'=>'',
+        'longitude'=>'',
+        'description'=>'La Casa di Alice si trova in zona centrale per la visita della Valdorcia
+        e Montalcino con sue cantine di Brunello.
+        Tutta ristrutturata e molto luminosa, può ospitare da 2 a 4 persone.
+        Interamente coperta da Wifi, ha due camere da letto matrimoniali, ciascuna con bagno privato, una cucina accessoriata un soggiorno relax con tv led, poltrone, camino a legna per le fredde sere d\'inverno.
+        Riscaldato a energia pulita e restaurato con materiali ecologici.',
+        'visible'=>'',
+        'price_per_night'=>175.00,
+        'rating'=>4.9,
+    ],
+    [
+        'title'=> 'Ledro Mountain Chalet',
+        'rooms_n'=>6,
+        'beds_n'=>3,
+        'bathroom'=>2,
+        'dimension'=>120,
+        'latitude'=>'',
+        'longitude'=>'',
+        'description'=>'Situato a Ledro, a 20 km da Riva del Garda, il Ledro Mountain Chalet offre una piscina all\'aperto e il WiFi gratuito.
+        Dotato di balcone e affacciato sul giardino, lo chalet include un’area salotto, una TV satellitare a schermo piatto e un bagno privato con bidet e asciugacapelli. Inoltre, comprende una cucina interamente attrezzata con lavastoviglie, microonde, frigorifero, piano cottura, bollitore e macchina da caffè.
+        Il Ledro Mountain Chalet propone colazioni à la carte e all\'italiana.
+        La struttura vanta una sauna.
+        Potrete usufruire di un barbecue e rilassarvi in giardino o nell’area salotto in comune.',
+        'visible'=>'',
+        'price_per_night'=>220.00,
+        'rating'=>5.0,
+    ]
+
+ ] ?>
