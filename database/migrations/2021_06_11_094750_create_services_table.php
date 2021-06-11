126f7beea74c0b36bc3133d82d4a4234fb4fe8cc
Maurizio Grasso <maurizio.grasso@gmail.com>
2021-06-11 12:18:22 +0200

    service underscore

--- database/migrations/2021_06_11_094750_create_services_table.php
+++ database/migrations/2021_06_11_094750_create_services_table.php
@@ -15,9 +15,9 @@ class CreateServicesTable extends Migration
     {
         Schema::create('services', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('icon');
-            $table->text('description');            
+            $table->string('service_name');
+            $table->string('service_icon');
+            $table->text('service_info');            
             $table->timestamps();
         });
     }
