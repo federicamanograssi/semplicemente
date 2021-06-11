9719306d8a0c2eeec910108eb5f5fb06078b1f02
Edoardo <edoardo.malfatto@hotmail.it>
2021-06-11 11:47:16 +0200

    seed registered_users caricato in config

---
+++ config/registered_users.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    [
+       'name'=> 'Edoardo',
+       'surname'=> 'Malfatti',
+       'email'=> 'edoardo@gmail.com',
+       'password'=> 'edoardo1',
+       'date_of_birth'=>'1997-02-21'
+    ],
+    [
+        'name'=> 'Yumo',
+        'surname'=> 'Shikato',
+        'email'=> 'yumo@gmail.com',
+        'password'=> 'yumo1',
+        'date_of_birth'=>'1998-05-15'
+
+     ],
+     [
+        'name'=> 'Federico',
+        'surname'=> 'Manigrasso',
+        'email'=> 'federico@gmail.com',
+        'password'=> 'federico1',
+        'date_of_birth'=>'1989-09-01'
+     ],
+     [
+        'name'=> 'Matteo',
+        'surname'=> 'Migliore',
+        'email'=> 'matteo@gmail.com',
+        'password'=> 'matteo1',
+        'date_of_birth'=>'1989-08-20'
+     ],
+     [
+        'name'=> 'Maurizia',
+        'surname'=> 'Grassi',
+        'email'=> 'maurizia@gmail.com',
+        'password'=> 'maurizia1',
+        'date_of_birth'=>'1988-03-30'
+     ]    
+]
+
+?>
