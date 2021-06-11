5857ccaece8d2a39639ab53956f5a9557925e379
Edoardo <edoardo.malfatto@hotmail.it>
2021-06-11 12:31:13 +0200

    rimosso "registered"

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('name',20)->nullable();
+            $table->string('surname',20)->nullable();
+            $table->string('email',50)->unique();
             $table->string('password');
+            $table->date('date_of_birth')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
