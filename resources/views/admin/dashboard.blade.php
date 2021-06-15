2430ff3702d4bc944229aaf3136423cab895a150
Federica <federica.manograssi@gmail.com>
2021-06-15 16:51:57 +0200

    layout base dashboard
    - navbar + link
    - sidebar

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,26 +1,6 @@
 {{-- view home quando ti sei loggato con successo --}}
-@extends('layouts.app')
+@extends('layouts.dashboard')
 
 @section('content')
-
-si sono io
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+QUI VA IL CONTENUTO DELLA DASHBOARD
 @endsection
