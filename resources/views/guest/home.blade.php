e65df07f67ec15d0433c91c752313b6a9888fd5d
Maurizio Grasso <maurizio.grasso@gmail.com>
2021-06-16 13:20:15 +0200

    Diventa un Host (Sezione Home Page)

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -5,16 +5,25 @@
     <main>
         <home-jumbo></home-jumbo>
 
-        <section class="home-section home-section--featured-apartments">
+        <section class="home-section featured-apartments">
             <h2 class="home-sections__heading heading--primary">
                 Le nostre scelte Top
             </h2>
             <apartments-list class="apartments-list--responsive"></apartments-list>
         </section>
 
-        <section class="home-section home-section--featured-locations">
+        
+        <section class="home-section become-host">
+            <div class="become-host__background">
+                <div class="become-host__text-box">
+                    <a class="become-host__cta" href="#">Diventa un Host</a>
+                </div>
+            </div>
+        </section>
 
-            <h2 class="home-section__heading heading-primary">
+        <section class="home-section featured-locations">
+
+            <h2 class="home-section__heading heading--primary">
                 Esplora le migliori destinazioni
             </h2>
             
