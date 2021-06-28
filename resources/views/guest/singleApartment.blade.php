4652ace06a08d5b3003d505b09a81363a406f506
Maurizio Grasso <maurizio.grasso@gmail.com>
2021-06-28 10:05:34 +0200

    Prima del checkout

--- resources/views/guest/singleApartment.blade.php
+++ resources/views/guest/singleApartment.blade.php
@@ -13,7 +13,6 @@
 @endsection
 
 @section('main')
-    {{-- <main> --}}
         
     {{-- @foreach ($apartments as $apartment) --}}
                     
@@ -44,7 +43,7 @@
 
 
         <div class="container">
-            <div class="form-container">
+            <div class="form-container" id="form-anchor">
                 <div class="left-container">
 
                     <section class="type-host">
@@ -247,18 +246,8 @@
     
             </single-chalet-map>
 
-                {{-- <section class="chalet-map">            
-                    <!-- 
-                        La mappa è provvisoriamente 
-                        integrata da Google Maps 
-                    -->
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d54471.2440560787!2d12.052418201662949!3d46.66978139530556!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47783435d247033f%3A0xdd3c30437b92e42b!2s32043%20Cortina%20d&#39;Ampezzo%20BL!5e1!3m2!1sit!2sit!4v1623660828144!5m2!1sit!2sit" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-                </section> --}}
-
-                {{-- inserire mappa appartamento --}}
             </section>
 
-            <hr>
 
             <section class="hosted-by">
                 <section class="type-host">
@@ -278,15 +267,11 @@
                 <section class="redirect-btn">
                     {{--Al click la pagina si ridireziona al form di inserimento messaggio --}}
 
-                    <button class="btn btn--primary host-btn">
+                    <a href="#form-anchor" class="btn btn--primary host-btn">
                         Contatta l'host ora
-                    </button>
+                    </a>
                 </section>
-
             </section>
-
-            {{-- @endforeach --}}
-        {{-- </main> --}}
 
         <section class="footer-top standard-padding">
             <div class="container">
