8dd49412ae5b2188f2a80ab13069810c4fa5665c
Federica <federica.manograssi@gmail.com>
2021-06-29 11:12:48 +0200

    Tabella sponsorizzata

--- resources/js/components/AdminSponsorshipsPage.vue
+++ resources/js/components/AdminSponsorshipsPage.vue
@@ -5,31 +5,63 @@
         <div class="row">
             <div class="col-12">
                 <h2>Le tue sponsorizzazioni</h2>
-                <thead class="table-light">
-                    <tr role="row">
-                        <th tabindex="0" rowspan="1" colspan="1">Appartamento</th>
-                        <th tabindex="0" rowspan="1" colspan="1">Status</th>
-                        <th tabindex="0" rowspan="1" colspan="1">Data fine</th>
-                        <th tabindex="0" rowspan="1" colspan="1">Prezzo</th>
-                    </tr>
-                </thead>
+                <h4>Qui puoi visualizzare tutte le tue sponsorizzazioni </h4>
 
-                <tbody>
-                    <tr role="row" v-for="sponsored_apartment in sponsored_apartments" :key="sponsored_apartment.id">
-                        <td class="text-left">{{sponsored_apartment.id}}</td>
-                        <!-- <td class="text-right">attivo/non attivo</td>
-                        <td class="text-right">{{sponsored_apartment.end_date}}</td>
-                        <td class="text-right">{{sponsored_apartment.amount}}</td> -->
-                    </tr>
-                </tbody>
+                <table class="table table-borderless">
+                    <thead>
+                        <tr role="row">
+                            <th scope="col">Appartamento</th>
+                            <th scope="col">Inizio</th>
+                            <th scope="col">Fine</th>
+                            <!-- <th scope="col">Attivo</th> -->
+                            <th scope="col">Spesa</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="sponsored_apartment in sponsored_apartments" :key="sponsored_apartment.id">
+                            <td class="text-left">{{sponsored_apartment.id}}</td>
+                            <td class="text-left">{{sponsored_apartment.start_date}}</td>
+                            <td class="text-left">{{sponsored_apartment.end_date}}</td>
+                            <!-- <td class="text-left">SI/NO</td> -->
+                            <td class="text-left">{{sponsored_apartment.amount}} €</td>
+                        </tr>
+                    </tbody>
+                </table>
+                
             </div>
         </div>
 
         <div class="row">
             <div class="col-12">
                 <h2>Metti in evidenza un appartamento</h2>
-                <button><a href="/admin/payment">vai al pagamento</a></button>
+                <button class="btn btn-warning btn-lg"><a href="/admin/payment">Sponsorizza</a></button>
 
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12">
+                <h2>Le tue transazioni</h2>
+
+                <table class="table table-borderless">
+                    <thead>
+                        <tr role="row">
+                            <th scope="col">Appartamento</th>
+                            <th scope="col">Data</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Importo</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="sponsored_apartment in sponsored_apartments" :key="sponsored_apartment.id">
+                            <td class="text-left">{{sponsored_apartment.id}}</td>
+                            <td class="text-left">{{sponsored_apartment.start_date}}</td>
+                            <td class="text-left">{{sponsored_apartment.status}}</td>
+                            <td class="text-left">{{sponsored_apartment.amount}} €</td>
+                        </tr>
+                    </tbody>
+                </table>
+                
             </div>
         </div>
 
@@ -63,4 +95,26 @@
     }
 
 </script>
+<style scoped>
+    table{
+        background-color: #d8af7f;
+        border-radius: .25rem;
+        width: 100%;
+        /* max-height: 25rem; */
+        overflow-x: auto;
+        /* overflow-y: auto; */
+    }
+    thead{
+        background-color: rgba(0,0,0,.03);
+        font-size: 1.7rem;
+    }
+    th,td{
+        padding:10px 20px;
+    }
+    @media (max-width: 768px){
+        table{
+            display: block;
+        }
+    }
+</style>
 
